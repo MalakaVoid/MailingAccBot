dc2ebf6696a91791f6b71a3224eb356a07ac2d3a
Daniil Azbukin <azbukin2003@gmail.com>
2023-08-01 21:12:27 +0300

    ADDED PRIVATE GROUPS FUNC

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 from pyrogram import types
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from globals import admins, chat_exmps, groups_to_add
-from txt_controller import get_groups, add_group
+from txt_controller import get_groups, add_group, remake_group_file
 from pyrogram import filters
 from StateClass import States
 from keys import API_ID, API_HASH
@@ -20,18 +20,37 @@ get_groups()
 
 
 with app:
+    delete_items = []
     for dialog in app.get_dialogs():
         if str(dialog.chat.username) in groups_to_add:
-            groups_to_add.remove(str(dialog.chat.username))
+            delete_items.append(str(dialog.chat.username))
             chat_exmps.append(dialog.chat)
+        else:
+            for each in groups_to_add:
+                if "https://t.me/+" in each:
+                    splt_arr = each.split(" ")
+                    if len(splt_arr) == 2:
+                        if dialog.chat.title == splt_arr[1]:
+                            chat_exmps.append(dialog.chat)
+                            delete_items.append(each)
+    remake_group_file(delete_items)
+    for i in delete_items:
+        groups_to_add.remove(i)
+
 
 with app:
     for group_name in groups_to_add:
+        group_url = group_name.split(" ")
         try:
-            chat = app.join_chat(group_name)
+            chat = app.join_chat(group_url[0])
             chat_exmps.append(chat)
+            if "https://t.me/+" in group_url[0]:
+                add_group(f"{group_url[0]} {chat.title}")
+            else:
+                add_group(f"{group_url[0]}")
         except Exception as e:
             print(str(e) + "\n")
+
 
 #handler
 @app.on_message(filters.user(admins) & filters.private)
@@ -60,7 +79,7 @@ async def admins_chat(client, message: types.Message):
             elif txt_input == "2":
                 msg_txt = "Группы:\n"
                 for e_chat in chat_exmps:
-                    msg_txt += f"ссылка: {e_chat.username} название: {e_chat.title}\n"
+                    msg_txt += f"название: {e_chat.title}\n"
                 await app.send_message(chat_id=message.chat.id,
                                        text=msg_txt)
                 await app.send_message(message.chat.id,
@@ -101,11 +120,16 @@ async def admins_chat(client, message: types.Message):
 
 
 async def join_chat_group(group_txt, admin_chat):         #Добавление в группы
-    group_name = added_line = group_txt.replace("https://t.me/", "")
+    group_name = group_txt
+    if "https://t.me/+" not in group_txt:
+        group_name = group_txt.replace("https://t.me/", "")
     try:
         chat = await app.join_chat(group_name)
         chat_exmps.append(chat)
-        add_group(group_txt)
+        if "https://t.me/+" in group_txt:
+            add_group(f"{group_txt} {chat.title}")
+        else:
+            add_group(group_txt)
     except Exception as e:
         ex_txt = str(e)
         await app.send_message(chat_id=admin_chat.id,
--- txt_controller.py
+++ txt_controller.py
@@ -5,12 +5,22 @@ def get_groups():
     f = open('groups.txt')
     for line in f:
         added_line = line.replace("\n", "")
-        added_line = added_line.replace("https://t.me/", "")
+        if "https://t.me/+" not in added_line:
+            added_line = added_line.replace("https://t.me/", "")
         groups_to_add.append(added_line)
+    f.close()
+    f = open('groups.txt', "w")
+    f.write("")
+    f.close()
+
+def remake_group_file(arr):
+    f = open('groups.txt', 'a')
+    for i in arr:
+        f.write(f"{i}\n")
     f.close()
 
 
 def add_group(group: str):
     f = open('groups.txt', 'a')
-    f.write(f"\n{group}")
+    f.write(f"{group}\n")
     f.close()
